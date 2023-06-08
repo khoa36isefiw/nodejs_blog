1588b12e6ee4aecfdee572871d76f80eee189838
khoa36isefiw <97817877+khoa36isefiw@users.noreply.github.com>
2023-06-08 09:28:48 +0700

    i'm learning nodejs of F8

---
+++ src/index.js
@@ -0,0 +1,66 @@
+// Hello World!
+const path = require('path');
+const express = require('express');  // express chính là thư viện mà ta đã tải về
+        // nếu ta gõ thẳng như vậy thì nó đi vào thư mục node_modules 
+        //nó tải xuống rồi nó lưu vào biến express
+
+// import require 
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');  // import thư viện của handlebars
+
+const app = express(); // function express đã được tạo sẵn trong express
+const port = 3000;   // run website ở port 3000
+
+
+
+
+// định nghĩa route
+// app.get('/', (req, res) => {        // => được hiểu là around function
+//   res.send('Hello World!')
+// })
+
+
+
+
+// quan sat request tu client   HTTP Logger
+app.use(morgan('combined'));
+
+
+// tải ảnh lên trong path/public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Tamplate engine
+app.engine('hbs', handlebars.engine({
+    // đổi tên handlebars thành hbs
+    extname: '.hbs',  // đuôi của handlebars  thành hbs
+
+})); // định nghĩa handlebars
+
+app.set('view engine', 'hbs');
+
+// path: trả về đường dẫn trong ứng dụng của mình
+//__dirname: name của ứng dụng của mình
+
+//console.log( "__dirname: "+ __dirname);
+//console.log( "PATH: ", path.join(__dirname, 'resources/views'));
+
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+// có thể chỉnh sửa route
+app.get('/', function(req, res) {
+    // /return res.send("Hello, World!");
+   res.render('home');
+})
+
+// tạo ra 1 route mới là news
+app.get('/news', function(req, res) {
+  res.render('news');
+})
+
+// 127.0.0.1 -> localhost
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
+
+// run app --> node appName.js
